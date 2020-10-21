381a7733f32326bdaab1b576ca2fbe1decb4e1dc
cscalia <cristian.scalia.88@gmail.com>
2020-10-20 22:09:57 -0300

    JsonLoader

---
+++ Assets/Code/JsonLoader/JsonLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IJsonLoader
+{
+    void LoadJson(string path, Action<string> callback);
+}
+
+public class JsonLoaderAsync : IJsonLoader
+{
+    public void LoadJson(string path, Action<string> callback)
+    {
+        LoadFileAsync(path, callback);
+    }
+
+    private async Task LoadFileAsync(string path, Action<string> OnLoaded)
+    {
+        using (FileStream SourceStream = File.Open(path, FileMode.Open))
+        {
+            using (StreamReader reader = new StreamReader(SourceStream, Encoding.UTF8))
+            {
+                string result = await reader.ReadToEndAsync();
+                OnLoaded(result);
+            }
+        }
+    }
+}
+
+
+public class JsonLoader : IJsonLoader
+{
+    public void LoadJson(string path, Action<string> callback)
+    {
+        string json = string.Empty;
+        if (File.Exists(path))
+        {
+            json = File.ReadAllText(path);
+        }
+        callback(json);
+    }
+}
