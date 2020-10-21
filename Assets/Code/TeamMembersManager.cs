b5ca49144ece7db3db4e2bbba97d8e8749da7588
cscalia <cristian.scalia.88@gmail.com>
2020-10-21 00:56:21 -0300

    Read the json and fill the table with the values.

---
+++ Assets/Code/TeamMembersManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Code.Table;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class TeamMembersManager : MonoBehaviour
+{
+    [SerializeField]
+    private UITable UITable;
+
+    private readonly string path = Application.streamingAssetsPath + "/JsonChallenge.json";
+
+    private void Awake()
+    {
+        LoadJson();
+    }
+
+    private void LoadJson()
+    {
+        IJsonLoader jsonLoader = new JsonLoader();
+        jsonLoader.LoadJson(path, OnJsonLoaded);
+    }
+
+    private void OnJsonLoaded(string json)
+    {
+        if (!string.IsNullOrEmpty(json))
+        {
+            UITable.Clear();
+            TeamMembersTableModel teamMembersTableModel = JsonConvert.DeserializeObject<TeamMembersTableModel>(json);
+            FillTable(teamMembersTableModel);
+        } 
+    }
+
+    private void FillTable(TeamMembersTableModel teamMembersTableModel)
+    {
+        UITable.AddTitle(teamMembersTableModel.Title);
+        UITable.AddHeaders(teamMembersTableModel.ColumnHeaders);
+        List<string> content = new List<string>();
+        foreach (var dic in teamMembersTableModel.Data)
+        {
+            foreach (var column in teamMembersTableModel.ColumnHeaders)
+            {
+                string text = String.Empty;
+                if(dic.TryGetValue(column, out object value))
+                {
+                    text = value.ToString();
+                }
+                content.Add(text);
+            }
+            UITable.AddContent(content.ToArray());
+            content.Clear();
+        }
+    }
+
+
+}
---
+++ Assets/Code/TeamMembersTableModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public class TeamMembersTableModel 
+{
+    public string Title;
+    public string[] ColumnHeaders;
+    public List<Dictionary<string,object>> Data;
+}
